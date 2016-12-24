9c30acb9e6cb998124aab96464ef3017aeece80a
artlenk.ru@gmail.com <artlenk.ru@gmail.com>
2016-12-24 09:41:34 +0400

    Created modal and gitignore file

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,5 +1,8 @@
 //= ../../bower_components/fancybox/source/jquery.fancybox.pack.js
 //= lib/countbox.js
+//= lib/bootstrap.min.js
+
+/* not active */
 // = lib/jquery.parallax.js
 // = lib/jquery.countTo.js
 // = lib/jquery.appear.js
