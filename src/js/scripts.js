aefde7cd78c8db060c4258adca20c2c1ab6cb606
alex-lenk <ay@remedio.pro>
2016-12-26 13:52:02 +0300

    Created faq block

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
      document.getElementById('current-year').innerHTML = currentYear;*/
 
     //Плавная прокрутка к якорям на странице
-    $("body").on('click', '[href*="#"]', function (e) {
+/*    $("body").on('click', '[href*="#"]', function (e) {
         var fixed_offset = 100;
         $('html,body').stop().animate({scrollTop: $(this.hash).offset().top - fixed_offset}, 1000);
         e.preventDefault();
-    });
+    });*/
 
     $(".fancybox").fancybox({
         openEffect: 'none',
